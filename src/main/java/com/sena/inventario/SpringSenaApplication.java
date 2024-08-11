19bbb2dd3c44718dd2fd80c4fbb676cf33079db8
Jesusyovanny <yovanny8610@gmail.com>
2024-08-11 10:53:54 -0500

    creacion de la estructura del proyecto y clases models

---
+++ src/main/java/com/sena/inventario/SpringSenaApplication.java
@@ -0,0 +1,13 @@
+package com.sena.inventario;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSenaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSenaApplication.class, args);
+	}
+
+}
